aba9f496c34b97d46c7621b4b264ca6f835a0c1c
Tony Gray <agray3@progressive.com>
2019-10-26 21:06:23 -0600

    Add files from prg for cmd

---
+++ .bashrc
@@ -0,0 +1,7 @@
+alias ll="pwd;ls -al"
+alias ast="git ast"
+alias acm="git acm"
+alias aco="git aco"
+alias abr="git abr"
+alias alo="git alo"
+
---
+++ GitSetup.bat
@@ -0,0 +1,7 @@
+@echo off
+rem C:\Program Files\Git\cmd
+doskey ast=git ast $*
+doskey acm=git acm $*
+doskey aco=git aco $*
+doskey abr=git abr $*
+doskey alo=git alo $*
