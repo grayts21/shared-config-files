cd45b9546d6a99ca032365d4b15f8c4343269510
Tony Gray <grayts21@gmail.com>
2019-11-22 20:58:23 -0700

    update at home

--- Deploy-Home.bat
+++ Deploy-Home.bat
@@ -1,13 +1,14 @@
 sed s/#HM#//g .gitconfig > hm.gitconfig
-copy hm.gitconfig C:\.gitconfig
+copy hm.gitconfig C:\Users\anthony\.gitconfig
 del hm.gitconfig
 
 sed '/#COMMENT#/d' GitSetup.bat > bash1.bashrc
 sed '/#DOS#/d' bash1.bashrc > bash2.bashrc
 sed 's/#BASH#//g' bash2.bashrc > bash.bashrc
-copy bash.bashrc C:\.bashrc
+copy bash.bashrc C:\Users\anthony\.bashrc
 del bash.bashrc
 del bash1.bashrc
+del bash2.bashrc
 
 sed '/#COMMENT#/d' GitSetup.bat > dos1.GitSetup
 sed '/#BASH#/d' dos1.GitSetup > dos2.GitSetup
--- Deploy-Progressive.bat
+++ Deploy-Progressive.bat
@@ -9,6 +9,7 @@ copy bash.bashrc U:\.bashrc
 copy bash.bashrc C:\CYGWIN\home\agray3\.bashrc
 del bash.bashrc
 del bash1.bashrc
+del bash2.bashrc
 
 sed '/#COMMENT#/d' GitSetup.bat > dos1.GitSetup
 sed '/#BASH#/d' dos1.GitSetup > dos2.GitSetup
